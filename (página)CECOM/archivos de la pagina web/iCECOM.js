e5dc0d09acecf8a7d532b38378cc57676aadc798
mszsorondo <52178350+mszsorondo@users.noreply.github.com>
2019-09-14 19:55:32 -0300

    falta mucho

---
+++ (página)CECOM/archivos de la pagina web/iCECOM.js
@@ -0,0 +1,16 @@
+let topnav = document.getElementById('topnav');
+let navchildren = topnav.children;
+console.log(navchildren)
+
+//herramientas y bibliografia
+let heYbi = navchildren[1];
+heYbi.addEventListener("click", window.alert('Falta poner contenido'))
+
+let publicaciones = navchildren[2];
+publicaciones.addEventListener("click", window.alert('Acá van a ir papers'))
+let integrantes = navchildren[3];
+let unirse = navchildren[4];
+
+
+
+
